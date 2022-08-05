083c8e091ddc598c91ce10f5143dd9d38025c338
unknown <hyeon31415@gmail.com>
2022-08-05 19:33:25 +0900

    modify calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,112 +1,93 @@
 
-
 let inputNumber='';
-let number = [];
-let operator = [];
+let numberArray = [];
+let operatorArray = [];
 
 function inputCalc(input) {
     const result = document.getElementById("result-input");
+
     // 초기화 함수
-    function init(){
+    function reset(){
         inputNumber='';
-        number = [];
-        operator = [];
+        numberArray = [];
+        operatorArray = [];
     }
-    // 처음 입력시 다른 값이 있을 경우 삭제
-    if( result.value != '' && inputNumber == '' && operator.length == 0) {
+
+    // 처음 입력 할 때 다른 값이 있을 경우 삭제
+    if( result.value != '' && inputNumber == '' && operatorArray.length == 0) {
         result.value='';
     }
 
-    // C 입력시 초기화
+    // 'C' 입력 시 초기화
     if(input == 'C') {
-        init();
+        reset();
         result.value='';
         return;
     }
+
     // 입력한 값 result-input에 출력
     result.value += input;
     
-    // 연산자 및 숫자 저장
+    // 연산자 입력 시 연산자 및 숫자 저장
     if(input == '+' || input =='-' || input == '*' || input == '/') {
+        // 숫자 없이 연산자를 눌렀을 때
         if(inputNumber =='') {
             result.value='잘못된 입력';
-            init();
+            reset();
             return;
         }
-        number.push(inputNumber);
+        numberArray.push(inputNumber);
         inputNumber = '';
-        operator.push(input);
+        operatorArray.push(input);
         return;
     }
-    // '=' 입력시 계산
+
+    // '=' 입력 시 계산 및 출력
     if(input == '='){
+        // 숫자 없이 '='를 눌렀을 때
         if(inputNumber =='') {
             result.value='잘못된 입력';
-            init();
+            reset();
             return;
         }
-        number.push(inputNumber);
+        numberArray.push(inputNumber);
         inputNumber = '';
         let ans = 0;
         let iter = 0;
-        while (number.length!=0){
+        while (numberArray.length!=0){
             if(iter==0) {
-                switch(operator[0]){
-                    case '+':
-                        ans = parseInt(number[0]) + parseInt(number[1]);
-                        number.splice(0,2);
-                        operator.shift();
-                        iter += 1;                        
-                        break;
-                        
-                    case '-':
-                        ans = parseInt(number[0]) - parseInt(number[1]);
-                        number.splice(0,2);
-                        operator.shift();
-                        iter += 1;  
-                        break;
-                    case '*':
-                        ans = parseInt(number[0]) * parseInt(number[1]);
-                        number.splice(0,2);
-                        operator.shift();
-                        iter += 1;  
-                        break;
-                    case '/':
-                        ans = parseInt(number[0]) / parseInt(number[1]);
-                        number.splice(0,2);
-                        operator.shift();
-                        iter += 1;  
-                        break;
-                }
+                ans = parseInt(numberArray[0]);
+                numberArray.shift();
+                iter += 1;
             }
-            switch(operator[0]){
+            switch(operatorArray[0]){
                 case '+':
-                    ans += parseInt(number[0]);
-                    number.shift();
-                    operator.shift();                        
+                    ans += parseInt(numberArray[0]);
+                    numberArray.shift();
+                    operatorArray.shift();                        
                     break;
                     
                 case '-':
-                    ans -= parseInt(number[0]);
-                    number.shift();
-                    operator.shift();
+                    ans -= parseInt(numberArray[0]);
+                    numberArray.shift();
+                    operatorArray.shift();
                     break;
                 case '*':
-                    ans *= parseInt(number[0]);
-                    number.shift();
-                    operator.shift();
+                    ans *= parseInt(numberArray[0]);
+                    numberArray.shift();
+                    operatorArray.shift();
                     break;
                 case '/':
-                    ans /= parseInt(number[0]);
-                    number.shift();
-                    operator.shift();
+                    ans /= parseInt(numberArray[0]);
+                    numberArray.shift();
+                    operatorArray.shift();
                     break;
             }
         }
         // 계산한 값 출력
         result.value = ans;
 
-        init();
+        reset();
         return;
     }
     // 숫자 입력시 문자열에 추가
